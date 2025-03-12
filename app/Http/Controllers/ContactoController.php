55a9adb2ee90b6d95717681e6cf9d8c57f3d7a9c
santos4510 <blackblaster07@gmail.com>
2025-03-12 17:17:58 +0000

    First laravel project

---
+++ app/Http/Controllers/ContactoController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ContactoController extends Controller
+{
+    public function index(){
+
+        echo "isto é a pagina contacto";
+    }
+}
